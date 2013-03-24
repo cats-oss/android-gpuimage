28f785ac211b038d3c41cee29d42d25c9f736dd7
Mohamed Ashraf <primary0@gmail.com>
2013-03-24 23:57:36 +0800

    Transparent GLSurfaceView background

--- library/src/jp/co/cyberagent/android/gpuimage/GPUImageRenderer.java
+++ library/src/jp/co/cyberagent/android/gpuimage/GPUImageRenderer.java
@@ -91,8 +91,10 @@ public class GPUImageRenderer implements Renderer, PreviewCallback {
 
     @Override
     public void onSurfaceCreated(final GL10 unused, final EGLConfig config) {
-        GLES20.glClearColor(0, 0, 0, 1);
-        GLES20.glDisable(GLES20.GL_DEPTH_TEST);
+        GLES20.glDisable(GL10.GL_DITHER);
+        GLES20.glClearColor(0,0,0,0);
+        GLES20.glEnable(GL10.GL_CULL_FACE);
+        GLES20.glEnable(GL10.GL_DEPTH_TEST);
         mFilter.init();
     }
 
