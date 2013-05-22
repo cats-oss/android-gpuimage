6a567bb0f1e7e0f72013b35b516486432a635eb9
javierpuntonet <javier@eisi.cc>
2013-05-22 11:34:57 +0300

    Update GPUImageTwoPassFilter.java

--- library/src/jp/co/cyberagent/android/gpuimage/GPUImageTwoPassFilter.java
+++ library/src/jp/co/cyberagent/android/gpuimage/GPUImageTwoPassFilter.java
@@ -4,11 +4,18 @@ import java.util.ArrayList;
 
 public class GPUImageTwoPassFilter extends GPUImageFilterGroup 
 {
-  public GPUImageTwoPassFilter(String firstVertexShader,String firstFragmentShader,String secondVertexShader,String secondFragmentShader) 
+	public GPUImageTwoPassFilter(String firstVertexShader,String firstFragmentShader,String secondVertexShader,String secondFragmentShader) 
 	{
-		super(new ArrayList<GPUImageFilter>());
+		super(createFilters(firstVertexShader, firstFragmentShader, secondVertexShader, secondFragmentShader));
+	}
+	
+	public static ArrayList<GPUImageFilter> createFilters(String firstVertexShader,String firstFragmentShader,String secondVertexShader,String secondFragmentShader)
+	{
+		ArrayList<GPUImageFilter> filters = new ArrayList<GPUImageFilter>();
 		
-		mFilters.add(new GPUImageFilter(firstVertexShader,firstFragmentShader));
-		mFilters.add(new GPUImageFilter(secondVertexShader,secondFragmentShader));
+		filters.add(new GPUImageFilter(firstVertexShader,firstFragmentShader));
+		filters.add(new GPUImageFilter(secondVertexShader,secondFragmentShader));
+		
+		return filters;
 	}
 }
