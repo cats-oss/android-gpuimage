55b7b7f5d9a8c756345820dbef6fef8b9417310a
Patrick Boos <boos_patrick@cyberagent.co.jp>
2013-05-24 10:55:16 +0900

    Formatting

--- library/src/jp/co/cyberagent/android/gpuimage/GPUImageBoxBlurFilter.java
+++ library/src/jp/co/cyberagent/android/gpuimage/GPUImageBoxBlurFilter.java
@@ -1,90 +1,83 @@
 package jp.co.cyberagent.android.gpuimage;
 
-import jp.co.cyberagent.android.gpuimage.GPUImageTwoPassTextureSamplingFilter;
+public class GPUImageBoxBlurFilter extends GPUImageTwoPassTextureSamplingFilter {
+    public static final String VERTEX_SHADER =
+            "attribute vec4 position;\n" +
+                    "attribute vec2 inputTextureCoordinate;\n" +
+                    "\n" +
+                    "uniform float texelWidthOffset; \n" +
+                    "uniform float texelHeightOffset; \n" +
+                    "\n" +
+                    "varying vec2 centerTextureCoordinate;\n" +
+                    "varying vec2 oneStepLeftTextureCoordinate;\n" +
+                    "varying vec2 twoStepsLeftTextureCoordinate;\n" +
+                    "varying vec2 oneStepRightTextureCoordinate;\n" +
+                    "varying vec2 twoStepsRightTextureCoordinate;\n" +
+                    "\n" +
+                    "void main()\n" +
+                    "{\n" +
+                    "gl_Position = position;\n" +
+                    "\n" +
+                    "vec2 firstOffset = vec2(1.5 * texelWidthOffset, 1.5 * texelHeightOffset);\n" +
+                    "vec2 secondOffset = vec2(3.5 * texelWidthOffset, 3.5 * texelHeightOffset);\n" +
+                    "\n" +
+                    "centerTextureCoordinate = inputTextureCoordinate;\n" +
+                    "oneStepLeftTextureCoordinate = inputTextureCoordinate - firstOffset;\n" +
+                    "twoStepsLeftTextureCoordinate = inputTextureCoordinate - secondOffset;\n" +
+                    "oneStepRightTextureCoordinate = inputTextureCoordinate + firstOffset;\n" +
+                    "twoStepsRightTextureCoordinate = inputTextureCoordinate + secondOffset;\n" +
+                    "}\n";
 
-public class GPUImageBoxBlurFilter extends GPUImageTwoPassTextureSamplingFilter 
-{
-  public static final String VERTEX_SHADER=
-      "attribute vec4 position;\n"+
-				  "attribute vec2 inputTextureCoordinate;\n"+
-				  "\n"+
-				  "uniform float texelWidthOffset; \n"+
-				  "uniform float texelHeightOffset; \n"+
-				  "\n"+
-				  "varying vec2 centerTextureCoordinate;\n"+
-				  "varying vec2 oneStepLeftTextureCoordinate;\n"+
-				  "varying vec2 twoStepsLeftTextureCoordinate;\n"+
-				  "varying vec2 oneStepRightTextureCoordinate;\n"+
-				  "varying vec2 twoStepsRightTextureCoordinate;\n"+
-				  "\n"+
-				  "void main()\n"+
-				  "{\n"+
-				  "gl_Position = position;\n"+
-				  "\n"+
-				  "vec2 firstOffset = vec2(1.5 * texelWidthOffset, 1.5 * texelHeightOffset);\n"+
-				  "vec2 secondOffset = vec2(3.5 * texelWidthOffset, 3.5 * texelHeightOffset);\n"+
-				  "\n"+
-				  "centerTextureCoordinate = inputTextureCoordinate;\n"+
-				  "oneStepLeftTextureCoordinate = inputTextureCoordinate - firstOffset;\n"+
-				  "twoStepsLeftTextureCoordinate = inputTextureCoordinate - secondOffset;\n"+
-				  "oneStepRightTextureCoordinate = inputTextureCoordinate + firstOffset;\n"+
-				  "twoStepsRightTextureCoordinate = inputTextureCoordinate + secondOffset;\n"+
-				  "}\n";
+    public static final String FRAGMENT_SHADER =
+            "precision highp float;\n" +
+                    "\n" +
+                    "uniform sampler2D inputImageTexture;\n" +
+                    "\n" +
+                    "varying vec2 centerTextureCoordinate;\n" +
+                    "varying vec2 oneStepLeftTextureCoordinate;\n" +
+                    "varying vec2 twoStepsLeftTextureCoordinate;\n" +
+                    "varying vec2 oneStepRightTextureCoordinate;\n" +
+                    "varying vec2 twoStepsRightTextureCoordinate;\n" +
+                    "\n" +
+                    "void main()\n" +
+                    "{\n" +
+                    "lowp vec4 fragmentColor = texture2D(inputImageTexture, centerTextureCoordinate) * 0.2;\n" +
+                    "fragmentColor += texture2D(inputImageTexture, oneStepLeftTextureCoordinate) * 0.2;\n" +
+                    "fragmentColor += texture2D(inputImageTexture, oneStepRightTextureCoordinate) * 0.2;\n" +
+                    "fragmentColor += texture2D(inputImageTexture, twoStepsLeftTextureCoordinate) * 0.2;\n" +
+                    "fragmentColor += texture2D(inputImageTexture, twoStepsRightTextureCoordinate) * 0.2;\n" +
+                    "\n" +
+                    "gl_FragColor = fragmentColor;\n" +
+                    "}\n";
 
-	public static final String FRAGMENT_SHADER=
-			"precision highp float;\n"+
-					"\n"+
-					"uniform sampler2D inputImageTexture;\n"+
-					"\n"+
-					"varying vec2 centerTextureCoordinate;\n"+
-					"varying vec2 oneStepLeftTextureCoordinate;\n"+
-					"varying vec2 twoStepsLeftTextureCoordinate;\n"+
-					"varying vec2 oneStepRightTextureCoordinate;\n"+
-					"varying vec2 twoStepsRightTextureCoordinate;\n"+
-					"\n"+
-					"void main()\n"+
-					"{\n"+
-					"lowp vec4 fragmentColor = texture2D(inputImageTexture, centerTextureCoordinate) * 0.2;\n"+
-					"fragmentColor += texture2D(inputImageTexture, oneStepLeftTextureCoordinate) * 0.2;\n"+
-					"fragmentColor += texture2D(inputImageTexture, oneStepRightTextureCoordinate) * 0.2;\n"+
-					"fragmentColor += texture2D(inputImageTexture, twoStepsLeftTextureCoordinate) * 0.2;\n"+
-					"fragmentColor += texture2D(inputImageTexture, twoStepsRightTextureCoordinate) * 0.2;\n"+
-					"\n"+
-					"gl_FragColor = fragmentColor;\n"+
-					"}\n";			
+    private float blurSize = 1f;
 
-	private float blurSize=1f;
+    public GPUImageBoxBlurFilter() {
+        this(1f);
+    }
 
-	public GPUImageBoxBlurFilter() 
-	{
-		this(1f);
-	}
+    public GPUImageBoxBlurFilter(float blurSize) {
+        super(VERTEX_SHADER, FRAGMENT_SHADER, VERTEX_SHADER, FRAGMENT_SHADER);
+        this.blurSize = blurSize;
+    }
 
-	public GPUImageBoxBlurFilter(float blurSize) 
-	{
-		super(VERTEX_SHADER, FRAGMENT_SHADER,VERTEX_SHADER, FRAGMENT_SHADER);
-		this.blurSize=blurSize;
-	}
-	
-	public void setBlurSize(float blurSize) {
-	        this.blurSize = blurSize;
-	        runOnDraw(new Runnable() {
-	            @Override
-	            public void run() {
-	                initTexelOffsets();
-	            }
-	        });
-	    }
+    public void setBlurSize(float blurSize) {
+        this.blurSize = blurSize;
+        runOnDraw(new Runnable() {
+            @Override
+            public void run() {
+                initTexelOffsets();
+            }
+        });
+    }
 
     @Override
-    public float getVerticalTexelOffsetRatio()
-	{
-		return blurSize;
-	}
+    public float getVerticalTexelOffsetRatio() {
+        return blurSize;
+    }
 
     @Override
-	public float getHorizontalTexelOffsetRatio()
-	{
-		return blurSize;
-	}
+    public float getHorizontalTexelOffsetRatio() {
+        return blurSize;
+    }
 }
--- library/src/jp/co/cyberagent/android/gpuimage/GPUImageCGAColorspaceFilter.java
+++ library/src/jp/co/cyberagent/android/gpuimage/GPUImageCGAColorspaceFilter.java
@@ -1,54 +1,52 @@
 package jp.co.cyberagent.android.gpuimage;
 
 public class GPUImageCGAColorspaceFilter extends GPUImageFilter {
-  
-  public static final String CGACOLORSPACE_FRAGMENT_SHADER = "" +
-	
-		"varying highp vec2 textureCoordinate;\n"+
-		"\n"+
-		"uniform sampler2D inputImageTexture;\n"+
-		"\n"+
-		"void main()\n"+
-		"{\n"+
-		"highp vec2 sampleDivisor = vec2(1.0 / 200.0, 1.0 / 320.0);\n"+
-		"//highp vec4 colorDivisor = vec4(colorDepth);\n"+
-		"\n"+
-		"highp vec2 samplePos = textureCoordinate - mod(textureCoordinate, sampleDivisor);\n"+
-		"highp vec4 color = texture2D(inputImageTexture, samplePos );\n"+
-		"\n"+
-		"//gl_FragColor = texture2D(inputImageTexture, samplePos );\n"+
-		"mediump vec4 colorCyan = vec4(85.0 / 255.0, 1.0, 1.0, 1.0);\n"+
-		"mediump vec4 colorMagenta = vec4(1.0, 85.0 / 255.0, 1.0, 1.0);\n"+
-		"mediump vec4 colorWhite = vec4(1.0, 1.0, 1.0, 1.0);\n"+
-		"mediump vec4 colorBlack = vec4(0.0, 0.0, 0.0, 1.0);\n"+
-		"\n"+
-		"mediump vec4 endColor;\n"+
-		"highp float blackDistance = distance(color, colorBlack);\n"+
-		"highp float whiteDistance = distance(color, colorWhite);\n"+
-		"highp float magentaDistance = distance(color, colorMagenta);\n"+
-		"highp float cyanDistance = distance(color, colorCyan);\n"+
-		"\n"+
-		"mediump vec4 finalColor;\n"+
-		"\n"+
-		"highp float colorDistance = min(magentaDistance, cyanDistance);\n"+
-		"colorDistance = min(colorDistance, whiteDistance);\n"+
-		"colorDistance = min(colorDistance, blackDistance); \n"+
-		"\n"+
-		"if (colorDistance == blackDistance) {\n"+
-		"finalColor = colorBlack;\n"+
-		"} else if (colorDistance == whiteDistance) {\n"+
-		"finalColor = colorWhite;\n"+
-		"} else if (colorDistance == cyanDistance) {\n"+
-		"finalColor = colorCyan;\n"+
-		"} else {\n"+
-		"finalColor = colorMagenta;\n"+
-		"}\n"+
-		"\n"+
-		"gl_FragColor = finalColor;\n"+
-		"}\n";
-	
-	public GPUImageCGAColorspaceFilter()
-	{
-		super(NO_FILTER_VERTEX_SHADER, CGACOLORSPACE_FRAGMENT_SHADER);
-	}
+
+    public static final String CGACOLORSPACE_FRAGMENT_SHADER = "" +
+            "varying highp vec2 textureCoordinate;\n" +
+            "\n" +
+            "uniform sampler2D inputImageTexture;\n" +
+            "\n" +
+            "void main()\n" +
+            "{\n" +
+            "highp vec2 sampleDivisor = vec2(1.0 / 200.0, 1.0 / 320.0);\n" +
+            "//highp vec4 colorDivisor = vec4(colorDepth);\n" +
+            "\n" +
+            "highp vec2 samplePos = textureCoordinate - mod(textureCoordinate, sampleDivisor);\n" +
+            "highp vec4 color = texture2D(inputImageTexture, samplePos );\n" +
+            "\n" +
+            "//gl_FragColor = texture2D(inputImageTexture, samplePos );\n" +
+            "mediump vec4 colorCyan = vec4(85.0 / 255.0, 1.0, 1.0, 1.0);\n" +
+            "mediump vec4 colorMagenta = vec4(1.0, 85.0 / 255.0, 1.0, 1.0);\n" +
+            "mediump vec4 colorWhite = vec4(1.0, 1.0, 1.0, 1.0);\n" +
+            "mediump vec4 colorBlack = vec4(0.0, 0.0, 0.0, 1.0);\n" +
+            "\n" +
+            "mediump vec4 endColor;\n" +
+            "highp float blackDistance = distance(color, colorBlack);\n" +
+            "highp float whiteDistance = distance(color, colorWhite);\n" +
+            "highp float magentaDistance = distance(color, colorMagenta);\n" +
+            "highp float cyanDistance = distance(color, colorCyan);\n" +
+            "\n" +
+            "mediump vec4 finalColor;\n" +
+            "\n" +
+            "highp float colorDistance = min(magentaDistance, cyanDistance);\n" +
+            "colorDistance = min(colorDistance, whiteDistance);\n" +
+            "colorDistance = min(colorDistance, blackDistance); \n" +
+            "\n" +
+            "if (colorDistance == blackDistance) {\n" +
+            "finalColor = colorBlack;\n" +
+            "} else if (colorDistance == whiteDistance) {\n" +
+            "finalColor = colorWhite;\n" +
+            "} else if (colorDistance == cyanDistance) {\n" +
+            "finalColor = colorCyan;\n" +
+            "} else {\n" +
+            "finalColor = colorMagenta;\n" +
+            "}\n" +
+            "\n" +
+            "gl_FragColor = finalColor;\n" +
+            "}\n";
+
+    public GPUImageCGAColorspaceFilter() {
+        super(NO_FILTER_VERTEX_SHADER, CGACOLORSPACE_FRAGMENT_SHADER);
+    }
 }
