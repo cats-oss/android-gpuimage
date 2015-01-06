49e50f578410aefc37f4c0a319e7564f5c27f71a
Zhanghu-PC <hoo.zhang@gmail.com>
2015-01-06 10:41:21 +0800

    fix formats and update comments

--- library/jni/yuv-decoder.c
+++ library/jni/yuv-decoder.c
@@ -36,23 +36,20 @@ JNIEXPORT void JNICALL Java_jp_co_cyberagent_android_gpuimage_GPUImageNativeLibr
                              Cr = yuv[cOff + 1];
                              if(Cr < 0) Cr += 127; else Cr -= 128;
                      }
-                     //==============================
-					 //R = 1.164Y + 2.018Cr;
-					 //G = 1.164Y - 0.813Cb - 0.391Cr;
-					 //B = 1.164Y + 1.596Cb;
-					 //==============================
-					 //===== Approximation ==========
-					 // R = 1.1640625Y + 2.015625Cr
-					 // G = 1.1640625Y - 0.8125Cb - 0.375Cr
-					 // B = 1.1640625Y + 1.59375Cb
-					 //==============================
+                     
+                     //ITU-R BT.601 conversion
+                     //
+                     //R = 1.164*(Y-16) + 2.018*(Cr-128);
+                     //G = 1.164*(Y-16) - 0.813*(Cb-128) - 0.391*(Cr-128);
+                     //B = 1.164*(Y-16) + 1.596*(Cb-128);
+                     //
                      Y = Y + (Y >> 3) + (Y >> 5) + (Y >> 7);
-					 R = Y + (Cr << 1) + (Cr >> 6);
-					 if(R < 0) R = 0; else if(R > 255) R = 255;
-					 G = Y - Cb + (Cb >> 3) + (Cb >> 4) - (Cr >> 1) + (Cr >> 3);
-					 if(G < 0) G = 0; else if(G > 255) G = 255;
-					 B = Y + Cb + (Cb >> 1) + (Cb >> 4) + (Cb >> 5);
-					 if(B < 0) B = 0; else if(B > 255) B = 255;
+                     R = Y + (Cr << 1) + (Cr >> 6);
+                     if(R < 0) R = 0; else if(R > 255) R = 255;
+                     G = Y - Cb + (Cb >> 3) + (Cb >> 4) - (Cr >> 1) + (Cr >> 3);
+                     if(G < 0) G = 0; else if(G > 255) G = 255;
+                     B = Y + Cb + (Cb >> 1) + (Cb >> 4) + (Cb >> 5);
+                     if(B < 0) B = 0; else if(B > 255) B = 255;
                      rgbData[pixPtr++] = 0xff000000 + (R << 16) + (G << 8) + B;
              }
     }
@@ -95,23 +92,20 @@ JNIEXPORT void JNICALL Java_jp_co_cyberagent_android_gpuimage_GPUImageNativeLibr
                              Cr = yuv[cOff + 1];
                              if(Cr < 0) Cr += 127; else Cr -= 128;
                      }
-                     //==============================
-					 //R = 1.164Y + 2.018Cr;
-					 //G = 1.164Y - 0.813Cb - 0.391Cr;
-					 //B = 1.164Y + 1.596Cb;
-					 //==============================
-					 //===== Approximation ==========
-					 // R = 1.1640625Y + 2.015625Cr
-					 // G = 1.1640625Y - 0.8125Cb - 0.375Cr
-					 // B = 1.1640625Y + 1.59375Cb
-					 //==============================
+                     
+                     //ITU-R BT.601 conversion
+                     //
+                     //R = 1.164*(Y-16) + 2.018*(Cr-128);
+                     //G = 1.164*(Y-16) - 0.813*(Cb-128) - 0.391*(Cr-128);
+                     //B = 1.164*(Y-16) + 1.596*(Cb-128);
+                     //
                      Y = Y + (Y >> 3) + (Y >> 5) + (Y >> 7);
-					 R = Y + (Cr << 1) + (Cr >> 6);
-					 if(R < 0) R = 0; else if(R > 255) R = 255;
-					 G = Y - Cb + (Cb >> 3) + (Cb >> 4) - (Cr >> 1) + (Cr >> 3);
-					 if(G < 0) G = 0; else if(G > 255) G = 255;
-					 B = Y + Cb + (Cb >> 1) + (Cb >> 4) + (Cb >> 5);
-					 if(B < 0) B = 0; else if(B > 255) B = 255;
+                     R = Y + (Cr << 1) + (Cr >> 6);
+                     if(R < 0) R = 0; else if(R > 255) R = 255;
+                     G = Y - Cb + (Cb >> 3) + (Cb >> 4) - (Cr >> 1) + (Cr >> 3);
+                     if(G < 0) G = 0; else if(G > 255) G = 255;
+                     B = Y + Cb + (Cb >> 1) + (Cb >> 4) + (Cb >> 5);
+                     if(B < 0) B = 0; else if(B > 255) B = 255;
                      rgbData[pixPtr++] = 0xff000000 + (B << 16) + (G << 8) + R;
              }
     }
