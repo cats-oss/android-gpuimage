97a386cdce0dbd82f5b681cb049c639d30f14c81
Takeshi Tsukamoto <itometeam@gmail.com>
2018-10-20 20:12:24 +0900

    Remove unused function

--- sample/src/main/java/jp/co/cyberagent/android/gpuimage/sample/utils/CameraLoader2.kt
+++ sample/src/main/java/jp/co/cyberagent/android/gpuimage/sample/utils/CameraLoader2.kt
@@ -13,23 +13,11 @@ class CameraLoader2 : CameraLoader() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getCameraWidth(): Int? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getCameraHeight(): Int? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getCameraOrientation(): Int? {
+    override fun getCameraOrientation(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun hasMultipleCamera(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun takePicture(onPictureTaken: (data: ByteArray) -> Unit) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
